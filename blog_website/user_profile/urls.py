2a6cde0fb4730fa5aa517004ab2fa8ec815507a7
sammanomar <>
2023-06-20 21:19:36 +0000

    user registration and login

--- blog_website/user_profile/urls.py
+++ blog_website/user_profile/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
-urlpatterns = [
+from user_profile.views import login_user
 
+from .views import *
+
+urlpatterns = [
+    path('login/', login_user, name='login'),
+    path('register_user/', register_user, name='register_user'),
 ]
