22c9ccb5421514964c2527f1f198bc7b32255bb0
sammanomar <>
2023-06-21 21:32:25 +0000

    design user profile UI

--- blog_website/user_profile/urls.py
+++ blog_website/user_profile/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('blog_login/', login_user, name='login'),
     path('blog_logout/', logout_user, name='logout'),
     path('register_user/', register_user, name='register_user'),
+    path('profile/', profile, name='profile'),
 ]
