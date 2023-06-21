68f84452183e150e2d90b13d2f62f60f0bed3a2c
sammanomar <>
2023-06-21 15:22:46 +0000

    finalize registration form

---
+++ blog_website/user_profile/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+
+from .models import User
+
+
+class UserRegistrationForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password", )
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        model = self.Meta.model
+        user = model.objects.filter(username__iexact=username)
+
+        if user.exists():
+            raise forms.ValidationError("A user with that name already exists")
+
+        return self.cleaned_data.get('username')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        model = self.Meta.model
+        user = model.objects.filter(email__iexact=email)
+
+        if user.exists():
+            raise forms.ValidationError(
+                "A user with that email already exists")
+
+        return self.cleaned_data.get('email')
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        confim_password = self.data.get('confirm_password')
+
+        if password != confim_password:
+            raise forms.ValidationError("Passwords do not match")
+
+        return self.cleaned_data.get('password')
--- blog_website/user_profile/urls.py
+++ blog_website/user_profile/urls.py
@@ -4,6 +4,7 @@ from user_profile.views import login_user
 
 from .views import *
 
+# 'login/' url is reserved in codeanywhere; therefore changed to 'blog_login/'
 urlpatterns = [
     path('blog_login/', login_user, name='login'),
     path('register_user/', register_user, name='register_user'),
