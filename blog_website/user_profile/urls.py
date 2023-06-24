785464f40e3c90a4f106dbffc04241f7a03d0a51
sammanomar <>
2023-06-24 14:26:30 +0000

    add mute and unmute notification feature

--- blog_website/user_profile/urls.py
+++ blog_website/user_profile/urls.py
@@ -17,4 +17,6 @@ urlpatterns = [
     path('follow_or_unfollow/<int:user_id>/',
          follow_or_unfollow_user, name='follow_or_unfollow_user'),
     path('user_notifications/', user_notifications, name='user_notifications'),
+    path('mute_or_unmute_user/<int:user_id>/',
+         mute_or_unmute_user, name='mute_or_unmute_user'),
 ]
