0e800ecfc355e4328a4c16cbb56f055ed5b4973f
sammanomar <>
2023-06-24 11:50:40 +0000

    add follow and unfollow users function

--- blog_website/user_profile/admin.py
+++ blog_website/user_profile/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import User
+from .models import Follow, User
 
 
 admin.site.register(User)
+admin.site.register(Follow)
